a61c37e0c270257aced319cfd7fd0ef6c30a291a
Udatya Deb <udatyadeb.cse2020@nsec.ac.in>
2022-04-10 19:27:28 +0530

    division added

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main()
 {
     int num1 = 0, num2 = 0;
     scanf("%d %d", &num1, &num2);
-    int num3 = num2 + num1;
+    float num3 = num2 * 1.0 / num1;
 
-    printf("The sum is: %d", num3);
+    printf("The quotient is: %f", num3);
 }
